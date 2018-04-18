18f33f4b2c1fb2b88189b1c93a46615d09fda024
Jakub Foltyn <foltyn.kuba@gmail.com>
2018-04-18 22:00:19 +0200

    Basic Spring Boot setup

---
+++ src/main/java/com/jfoltyn/forsetiserver/ForsetiServer.java
@@ -0,0 +1,11 @@
+package com.jfoltyn.forsetiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForsetiServer {
+   public static void main(String[] args) {
+      SpringApplication.run(ForsetiServer.class, args);
+   }
+}
