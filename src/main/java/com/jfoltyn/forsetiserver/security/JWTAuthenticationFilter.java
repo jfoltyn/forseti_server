006f9f0de9d63eea8697b8fbc7303dd35865e937
Jakub Foltyn <foltyn.kuba@gmail.com>
2018-05-01 16:42:14 +0200

    Returning JWT and username in response body

--- src/main/java/com/jfoltyn/forsetiserver/security/JWTAuthenticationFilter.java
+++ src/main/java/com/jfoltyn/forsetiserver/security/JWTAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.jfoltyn.forsetiserver.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jfoltyn.forsetiserver.user.User;
 import io.jsonwebtoken.Jwts;
@@ -17,8 +18,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.jfoltyn.forsetiserver.security.SecurityConstants.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
    private AuthenticationManager authenticationManager;
@@ -51,11 +55,24 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                            FilterChain chain,
                                            Authentication auth) throws IOException, ServletException {
 
+      String username = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername();
       String token = Jwts.builder()
-            .setSubject(((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername())
+            .setSubject(username)
             .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
             .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
             .compact();
       res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+
+      String responseJson = buildResponseBody(username, token);
+
+      res.setContentType(APPLICATION_JSON_VALUE);
+      res.getWriter().write(responseJson);
+   }
+
+   private String buildResponseBody(String username, String token) throws JsonProcessingException {
+      Map<String, String> response = new HashMap<>();
+      response.put("username", username);
+      response.put("authorization", TOKEN_PREFIX + token);
+      return new ObjectMapper().writeValueAsString(response);
    }
 }
