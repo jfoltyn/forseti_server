74547019a2f082bc89fc3edbe71126d4952e3dd4
Foltyn, Jakub <jakub.foltyn@sabre.com>
2018-05-05 00:54:13 +0200

    Added Swagger annotation to allow passing header from UI

--- src/main/java/com/jfoltyn/forsetiserver/controllers/AdminRestController.java
+++ src/main/java/com/jfoltyn/forsetiserver/controllers/AdminRestController.java
@@ -5,6 +5,7 @@ import com.jfoltyn.forsetiserver.admin.AdminService;
 import com.jfoltyn.forsetiserver.admin.UsersList;
 import com.jfoltyn.forsetiserver.user.User;
 import com.jfoltyn.forsetiserver.user.UserService;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 import static com.jfoltyn.forsetiserver.controllers.ServiceDictionary.ADMIN_USER;
+import static com.jfoltyn.forsetiserver.security.SecurityConstants.AUTHORIZATION_HEADER;
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -28,11 +30,13 @@ public class AdminRestController {
    private AccountNumberService accountNumberService;
 
    @RequestMapping(value = ADMIN_USER + "{username}", method = GET)
+   @ApiImplicitParam(name = AUTHORIZATION_HEADER, value = AUTHORIZATION_HEADER, required = true, dataType = "String", paramType = "header")
    public User getUser(@PathVariable("username") String username) {
       return adminService.getUser(username);
    }
 
    @RequestMapping(value = ADMIN_USER + "{username}", method = DELETE)
+   @ApiImplicitParam(name = AUTHORIZATION_HEADER, value = AUTHORIZATION_HEADER, required = true, dataType = "String", paramType = "header")
    public UsersList deleteUser(@PathVariable("username") String username) {
       adminService.removeUser(username);
       return adminService.getUsersList();
